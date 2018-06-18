6754d2d90d59cc179b3bf6676f7c9b1495c90281
Doogle <fdunigan11@gmail.com>
2018-06-18 13:26:35 -0400

    initial site changes

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,31 @@
+$("button").on("click", function() {
+    var musician = $(this).attr("data-musician");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        musician + "&api_key=nMb2MXAWMMnSaR0o3C8CkWcrig7hVTc7&limit=10";
+  
+    $.ajax({
+     url: queryURL,
+     method: "GET"
+    })
+    .then(function(response) {
+            
+    var results = response.data;
+  
+    for (var i = 0; i < results.length; i++) {
+  
+    var musicianDiv = $("<div>");
+  
+    var p = $("<p>").text("Rating: " + results[i].rating);
+  
+    var musicianImage = $("<img>");
+              
+    musicianImage.attr("src", results[i].images.fixed_height.url);
+  
+    musicianDiv.append(p);
+    musicianDiv.append(musicianImage);
+  
+    $("#gifs-appear-here").prepend(musicianDiv);
+    
+}
+});
+});
