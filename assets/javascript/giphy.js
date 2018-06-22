911be4408f9ae8fde4db563645a8cee7447ad08a
Doogle <fdunigan11@gmail.com>
2018-06-22 18:09:04 -0400

    final changes showing static gifs on button click and then becoming animated once clicked by the user

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,64 +2,83 @@ var musicians = ["freddie mercury", "neil peart", "john bonham", "jimi hendrix",
 
 function displayMusicianGif() {
     var musician = $(this).attr("data-musician");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
-    musician + "&api_key=nMb2MXAWMMnSaR0o3C8CkWcrig7hVTc7&limit=10";
-  
-    $.ajax({
-     url: queryURL,
-     method: "GET"
-    })
-    .then(function(response) {
-        console.log(queryURL);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        musician + "&api_key=nMb2MXAWMMnSaR0o3C8CkWcrig7hVTc7&limit=10";
 
-        console.log(response);
-            
-    var results = response.data;
-  
-    for (var i = 0; i < results.length; i++) {
-  
-    var musicianDiv = $("<div>");
-  
-    var p = $("<p>").text("Rating: " + results[i].rating);
-  
-    var musicianImage = $("<img>");
-              
-    musicianImage.attr("src", results[i].images.fixed_height.url);
-  
-    musicianDiv.append(p);
-    musicianDiv.append(musicianImage);
-  
-    $("#gifs-appear-here").prepend(musicianDiv);
-    
-};
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(queryURL);
+
+            console.log(response);
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var musicianDiv = $("<div>");
+
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                var musicianImage = $("<img>");
+
+                musicianImage.attr("src", results[i].images.fixed_height_still.url);
+                musicianImage.attr("data-still", results[i].images.fixed_height_still.url);
+                musicianImage.attr("data-animate", results[i].images.fixed_height.url);
+                musicianImage.attr("data-state", "still");
+                musicianImage.addClass("musicianImage");
+
+                musicianDiv.append(p);
+                musicianDiv.append(musicianImage);
+
+                $("#gifs-appear-here").prepend(musicianDiv);
+
+            };
+            $(".musicianImage").on("click", function () {
+                var state = $(this).attr("data-state");
+                console.log(state);
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        });
 };
 
 function renderButtons() {
 
-$("#buttons-view").empty();
+    $("#buttons-view").empty();
 
     for (var i = 0; i < musicians.length; i++) {
 
         var b = $("<button>");
-          
+
         b.addClass("musician-btn");
-          
+
         b.attr("data-musician", musicians[i]);
-          
+
         b.text(musicians[i]);
-          
+
         $("#buttons-view").append(b);
+
     }
 }
 
-$("#add-musician").on("click", function(event) {
+$("#add-musician").on("click", function (event) {
 
-event.preventDefault();
-        
+    event.preventDefault();
+
     var musician = $("#musician-input").val().trim();
 
     musicians.push(musician);
+
+    console.log(musicians);
 
     renderButtons();
 
